0c5528db0c01cf25783aab7fca8123a58a72a3ac
Naihe <64341459+239144498@users.noreply.github.com>
2024-12-09 12:40:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ ocr = ddddocr.DdddOcr()
 app = FastAPI()
 
 # 静态文件目录
-os.makedirs(".static", exist_ok=True)
+os.makedirs("static", exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 cache = {}
 
